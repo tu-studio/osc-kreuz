4bf5210daffcb8ba0e45365d6a06c61bb274d088
Max Weidauer <max+git@weidauer.art>
2024-08-31 04:05:42 +0200

    also added tests for positional osc messages

--- tests/test_osc_kreuz.py
+++ tests/test_osc_kreuz.py
@@ -1,3 +1,4 @@
+from itertools import product
 import logging
 import math
 import os
@@ -10,10 +11,18 @@ from time import sleep, time
 from click.testing import CliRunner
 from oscpy.client import OSCClient
 
+from osc_kreuz import coordinates
 from osc_kreuz.osc_kreuz import main, read_config, read_config_option, signal_handler
-from osckreuz_listener import SeamlessListener
+from osckreuz_listener import SeamlessListener, Source
 import osc_kreuz.str_keys_conventions as skc
 import osc_kreuz.coordinates
+from osc_kreuz.coordinates import (
+    CoordinateKey,
+    CoordinatePolar,
+    CoordinatePolarRadians,
+    CoordinateSystemType,
+)
+import numpy as np
 
 test_config = Path(__file__).parent / "assets" / "config_test.yml"
 config = read_config(test_config)
@@ -45,20 +54,6 @@ def build_gain_test_path(path, renderer_in_path, source_index_in_path):
     return (path, args, source_index, renderer_index, val)
 
 
-# def build_coordinate_test_path(path, source_index_in_path):
-#     source_index = random.randint(0, n_sources - 1)
-#     args = []
-#     if not source_index_in_path:
-#         args.append(source_index + 1)
-#     for coordinate_format_str in osc_kreuz.coordinates.get_all_coordinate_formats():
-#         coordinate_system, coordinate_keys = osc_kreuz.coordinates.parse_coordinate_format(coordinate_format_str)
-#         if coordinate_system
-#         # TODO continue here
-#         # TODO how to handle incomplete paths here?
-
-#     return (path, args, source_index, (x_expected, y_expected, z_expected))
-
-
 def build_all_gain_paths():
     paths = []
 
@@ -83,28 +78,39 @@ def build_all_gain_paths():
 
 
 def test_gains():
+    # cli runner to run the actual osckreuz
     runner = CliRunner()
     osc_kreuz_thread = Thread(
         target=runner.invoke,
         args=(main, ["-c", str(test_config.absolute().resolve())]),
     )
     osc_kreuz_thread.start()
+
     sleep(0.5)
+
+    # listener to receive osc from osc kreuz
     listener = SeamlessListener(
         n_sources, "127.0.0.1", 9876, "127.0.0.1", 4999, "osckreuz_test"
     )
+
+    # sender to send updates to osc-kreuz
     sender = OSCClient("127.0.0.1", 4455)
 
+    # something changed to check if the update sent to the osc-kreuz also came back
     something_changed = Event()
 
     def osc_callback(*args):
         something_changed.set()
 
+    # setup listener
     listener.register_gain_callback(osc_callback)
     listener.register_position_callback(osc_callback)
     listener.start_listening()
 
-    sleep(1)
+    # sleep to wait for initial barrage of updates
+    sleep(0.5)
+
+    # test a lot of different paths
     for path, args, source_index, renderer, expected_val in build_all_gain_paths():
         something_changed.clear()
 
@@ -112,7 +118,7 @@ def test_gains():
         val = something_changed.wait(5)
 
         assert val == True
-        # logging.info(listener.sources)
+        # standard epsilon is to fine grained
         assert math.isclose(
             listener.sources[source_index].gain[renderer], expected_val, rel_tol=1e-06
         )
@@ -123,5 +129,268 @@ def test_gains():
     logging.info("killed osc-kreuz")
 
 
+def random_coords_for_coordinate_type(
+    coordinate_system: CoordinateSystemType, coordinate_key: CoordinateKey
+):
+    if coordinate_system == CoordinateSystemType.Cartesian:
+        return (random.random() - 0.5) * 10
+    elif coordinate_key == CoordinateKey.d:
+        return random.random() * 5
+    if coordinate_system == CoordinateSystemType.Polar:
+        return (random.random() - 0.5) * 360
+    else:
+        return (random.random() - 0.5) * 2 * np.pi
+
+
+def build_coordinate_test_path(
+    path,
+    coordinate_format_str,
+    source_index,
+    source_index_in_path,
+    initial_position: Source,
+):
+    args = []
+    if not source_index_in_path:
+        args.append(source_index + 1)
+    x = (random.random() - 0.5) * 10
+
+    path = path.format(val=coordinate_format_str, idx=source_index + 1)
+
+    initial_coordinate = osc_kreuz.coordinates.CoordinateCartesian(
+        initial_position.x, initial_position.y, initial_position.z
+    )
+    # for coordinate_format_str in osc_kreuz.coordinates.get_all_coordinate_formats():
+    coordinate_system, coordinate_keys = osc_kreuz.coordinates.parse_coordinate_format(
+        coordinate_format_str
+    )
+    vals = [
+        random_coords_for_coordinate_type(coordinate_system, key)
+        for key in coordinate_keys
+    ]
+
+    args.extend(vals)
+
+    if coordinate_system == osc_kreuz.coordinates.CoordinateSystemType.Cartesian:
+        initial_coordinate.set_coordinates(coordinate_keys, vals)
+    else:
+        conv_coordinates = initial_coordinate.convert_to(coordinate_system)
+        if coordinate_system == osc_kreuz.coordinates.CoordinateSystemType.Polar:
+            coords = osc_kreuz.coordinates.CoordinatePolar(*conv_coordinates)
+        else:
+            coords = osc_kreuz.coordinates.CoordinatePolarRadians(*conv_coordinates)
+
+        coords.set_coordinates(coordinate_keys, vals)
+
+        cart_coordinates = coords.convert_to(
+            osc_kreuz.coordinates.CoordinateSystemType.Cartesian
+        )
+        initial_coordinate.set_all(*cart_coordinates)
+
+    return (path, args, initial_coordinate.get_all())
+
+
+def test_full_positions():
+    # cli runner to run the actual osckreuz
+    runner = CliRunner()
+    osc_kreuz_thread = Thread(
+        target=runner.invoke,
+        args=(main, ["-c", str(test_config.absolute().resolve())]),
+    )
+    osc_kreuz_thread.start()
+
+    sleep(0.5)
+
+    # listener to receive osc from osc kreuz
+    listener = SeamlessListener(
+        n_sources, "127.0.0.1", 9876, "127.0.0.1", 4999, "osckreuz_test"
+    )
+
+    # sender to send updates to osc-kreuz
+    sender = OSCClient("127.0.0.1", 4455)
+
+    # something changed to check if the update sent to the osc-kreuz also came back
+    something_changed = Event()
+
+    def osc_callback(*args):
+        something_changed.set()
+
+    # setup listener
+    listener.register_gain_callback(osc_callback)
+    listener.register_position_callback(osc_callback)
+    listener.start_listening()
+
+    # sleep to wait for initial barrage of updates
+    sleep(0.5)
+
+    # test a lot of different paths
+    paths = [
+        (path, False) for path in skc.osc_paths[skc.OscPathType.Position]["base"]
+    ] + [(path, True) for path in skc.osc_paths[skc.OscPathType.Position]["extended"]]
+    for (path, source_index_in_path), pos_format in product(
+        paths, ["xyz", "aed", "aedrad"] * 500
+    ):
+        something_changed.clear()
+        source_index = random.randint(0, n_sources - 1)
+
+        path, args, expected_xyz = build_coordinate_test_path(
+            path,
+            pos_format,
+            source_index,
+            source_index_in_path,
+            listener.sources[source_index],
+        )
+        before_xyz = (
+            listener.sources[source_index].x,
+            listener.sources[source_index].y,
+            listener.sources[source_index].z,
+        )
+        sender.send_message(path.encode(), args)
+        val = something_changed.wait(5)
+
+        assert val == True
+        # standard epsilon is to fine grained
+
+        assert np.allclose(
+            (
+                listener.sources[source_index].x,
+                listener.sources[source_index].y,
+                listener.sources[source_index].z,
+            ),
+            expected_xyz,
+            rtol=1e-3,
+        )
+
+    logging.info("unsubscribing")
+    listener.unsubscribe_from_osc_kreuz()
+    signal_handler()
+    logging.info("killed osc-kreuz")
+
+
+def test_positions():
+    # cli runner to run the actual osckreuz
+    runner = CliRunner()
+    osc_kreuz_thread = Thread(
+        target=runner.invoke,
+        args=(main, ["-c", str(test_config.absolute().resolve())]),
+    )
+    osc_kreuz_thread.start()
+
+    sleep(0.5)
+
+    # listener to receive osc from osc kreuz
+    listener = SeamlessListener(
+        n_sources, "127.0.0.1", 9876, "127.0.0.1", 4999, "osckreuz_test"
+    )
+
+    # sender to send updates to osc-kreuz
+    sender = OSCClient("127.0.0.1", 4455)
+
+    # something changed to check if the update sent to the osc-kreuz also came back
+    something_changed = Event()
+
+    def osc_callback(*args):
+        something_changed.set()
+
+    # setup listener
+    listener.register_gain_callback(osc_callback)
+    listener.register_position_callback(osc_callback)
+    listener.start_listening()
+
+    # sleep to wait for initial barrage of updates
+    sleep(0.5)
+
+    # test a lot of different paths
+    paths = [
+        (path, False) for path in skc.osc_paths[skc.OscPathType.Position]["base"]
+    ] + [(path, True) for path in skc.osc_paths[skc.OscPathType.Position]["extended"]]
+    for (path, source_index_in_path), pos_format in product(
+        paths, osc_kreuz.coordinates.get_all_coordinate_formats()
+    ):
+        something_changed.clear()
+        source_index = random.randint(0, n_sources - 1)
+
+        path, args, expected_xyz = build_coordinate_test_path(
+            path,
+            pos_format,
+            source_index,
+            source_index_in_path,
+            listener.sources[source_index],
+        )
+        before_xyz = (
+            listener.sources[source_index].x,
+            listener.sources[source_index].y,
+            listener.sources[source_index].z,
+        )
+        sender.send_message(path.encode(), args)
+        val = something_changed.wait(5)
+
+        assert val == True
+        # standard epsilon is to fine grained
+
+        try:
+            assert np.allclose(
+                (
+                    listener.sources[source_index].x,
+                    listener.sources[source_index].y,
+                    listener.sources[source_index].z,
+                ),
+                expected_xyz,
+                rtol=1e-4,
+            )
+        except AssertionError:
+            print(f"checking pos path {path}, {pos_format}")
+
+            print("before:   ", *before_xyz)
+            print(
+                "after:    ",
+                listener.sources[source_index].x,
+                listener.sources[source_index].y,
+                listener.sources[source_index].z,
+            )
+            print("expected: ", *expected_xyz)
+            print("args: ", *args)
+
+            import matplotlib.pyplot as plt
+
+            fig = plt.figure()
+            ax = fig.add_subplot(1, 1, 1, projection="3d")
+            ax.plot(*zip(before_xyz, (0, 0, 0)), label="before")
+            ax.plot(
+                *zip(
+                    (
+                        listener.sources[source_index].x,
+                        listener.sources[source_index].y,
+                        listener.sources[source_index].z,
+                    ),
+                    (0, 0, 0),
+                ),
+                label="after",
+            )
+            ax.plot(*zip(expected_xyz, (0, 0, 0)), label="expected")
+
+            csystem, ckeys = coordinates.parse_coordinate_format(pos_format)
+            if csystem == CoordinateSystemType.PolarRadians:
+                c = CoordinatePolarRadians(0, 0, 1)
+            else:
+                c = CoordinatePolar(0, 0, 1)
+            c.set_coordinates(ckeys, args[-len(ckeys) :])
+            ax.plot(
+                *zip(c.convert_to(CoordinateSystemType.Cartesian), (0, 0, 0)),
+                label="change",
+            )
+
+            plt.legend()
+            plt.title(f"{pos_format}: {c.get_all()}")
+            plt.show()
+
+            raise
+
+    logging.info("unsubscribing")
+    listener.unsubscribe_from_osc_kreuz()
+    signal_handler()
+    logging.info("killed osc-kreuz")
+
+
 if __name__ == "__main__":
-    test_gains()
+    # test_gains()
+    test_full_positions()
