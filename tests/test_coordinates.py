739d6ce8a06b0a10abea6d2a64ea716ae2fe61ea
Max Weidauer <max+git@weidauer.art>
2024-08-31 04:08:10 +0200

    testet coordinate conversions with pyfar coordinates as comparison

--- tests/test_coordinates.py
+++ tests/test_coordinates.py
@@ -1,8 +1,14 @@
+import random
 from osc_kreuz.coordinates import (
+    CoordinatePolar,
+    CoordinatePolarRadians,
     CoordinateSystemType,
     CoordinateKey,
     parse_coordinate_format,
 )
+
+from pyfar import Coordinates
+import numpy as np
 
 
 def test_format_str_parsing():
@@ -48,5 +54,59 @@ def test_format_str_parsing():
         print()
 
 
+def test_spherical_coordinates():
+    for _ in range(10000):
+
+        c = CoordinatePolarRadians(
+            (random.random() - 0.5) * 2 * np.pi,
+            (random.random() - 0.5) * 2 * np.pi,
+            (random.random()) * 5,
+        )
+        a, e, d = c.get_all()
+        c_compare = Coordinates.from_spherical_elevation(a, e, d)
+        print(c_compare.cartesian[0], c.convert_to(CoordinateSystemType.Cartesian))
+        assert np.allclose(
+            c.convert_to(CoordinateSystemType.Cartesian), c_compare.cartesian[0]
+        )
+
+
+def test_spherical_coordinates_setting_azim():
+    for _ in range(10000):
+        azim_new = (random.random() - 0.5) * 4 * np.pi
+
+        c = CoordinatePolarRadians(
+            (random.random() - 0.5) * 2 * np.pi,
+            (random.random() - 0.5) * 2 * np.pi,
+            (random.random()) * 5,
+        )
+
+        c.set_coordinates([CoordinateKey.a], [azim_new])
+        a, e, d = c.get_all()
+        c_compare = Coordinates.from_spherical_elevation(azim_new, e, d)
+        print(c_compare.cartesian[0], c.convert_to(CoordinateSystemType.Cartesian))
+        assert np.allclose(
+            c.convert_to(CoordinateSystemType.Cartesian), c_compare.cartesian[0]
+        )
+
+
+def test_spherical_coordinates_degree():
+    for _ in range(10000):
+
+        c = CoordinatePolar(
+            (random.random() - 0.5) * 720,
+            (random.random() - 0.5) * 720,
+            (random.random()) * 5,
+        )
+        a, e, d = c.get_all()
+        c_compare = Coordinates.from_spherical_elevation(
+            a / 180 * np.pi, e / 180 * np.pi, d
+        )
+        print(c_compare.cartesian[0], c.convert_to(CoordinateSystemType.Cartesian))
+
+        assert np.allclose(
+            c.convert_to(CoordinateSystemType.Cartesian), c_compare.cartesian[0]
+        )
+
+
 if __name__ == "__main__":
-    test_format_str_parsing()
+    test_spherical_coordinates_degree()
